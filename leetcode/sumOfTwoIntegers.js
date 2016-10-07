e214a485034cfea18bd93dd2d2e1637f1c27382c
chenhaoyang <chenhaoyang0529@gmail.com>
2016-10-08 01:39:19 +0800

    add a function

--- leetcode/sumOfTwoIntegers.js
+++ leetcode/sumOfTwoIntegers.js
@@ -3,7 +3,9 @@
  * @param {number} b
  * @return {number}
  */
-var getSum = function(a, b) {
+
+// function one
+var getSum1 = function(a, b) {
   var carry = 0,
     length = 32,
     result = 0,
@@ -18,4 +20,14 @@ var getSum = function(a, b) {
   }
 
   return result;
+};
+
+// function two
+var getSum2 = function(a, b) {
+  if (b === 0) {
+    return a;
+  }
+  var sum = a ^ b,
+    carry = (a & b) << 1;
+  return getSum(sum, carry);
 };
