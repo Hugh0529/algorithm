d47e530edb86220076a932433a98bc812b6bc204
chenhaoyang <chenhaoyang0529@gmail.com>
2016-10-23 02:26:16 +0800

    Longest Palindrome

--- leetcode/longestPalindrome.js
+++ leetcode/longestPalindrome.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
+    var set = new Set(),
+        length = s.length,
+        item,
+        i,
+        odd;
     
+    for (i = length - 1; i >=0; i--) {
+        item = s.charAt(i);
+        if (set.has(item)) {
+            set.delete(item);
+        } else {
+            set.add(item);
+        }
+    }
+    
+    odd = set.size;
+    return length - (odd === 0 ? 0 : odd -1 );
 };
