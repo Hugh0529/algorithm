d6d38ce7cd6c14eff705878ac3b4f32104afc903
chenhaoyang <chenhaoyang0529@gmail.com>
2016-10-25 00:45:42 +0800

    update

--- leetcode/containsDuplicate.js
+++ leetcode/containsDuplicate.js
@@ -3,6 +3,16 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    var set = new Set(nums);
-    return nums.length !== set.size;
+    var set = new Set(),
+              i;
+        
+    for (i = nums.length - 1; i >= 0; i--) {
+        if (set.has(nums[i])) {
+            return true;
+        } else {
+            set.add(nums[i]);
+        }
+    }
+    
+    return false;
 };
