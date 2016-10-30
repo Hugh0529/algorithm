446499af535866b67749584b7ec4e0547a77a4ec
chenhaoyang <chenhaoyang0529@gmail.com>
2016-10-30 23:06:14 +0800

    update code style

--- leetcode/convertANumberToHexadecimal.js
+++ leetcode/convertANumberToHexadecimal.js
@@ -4,7 +4,7 @@
  */
 var toHex = function(num) {
     if (num === 0) {
-       return "0";
+       return '0';
     }
 
     var map = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'],
