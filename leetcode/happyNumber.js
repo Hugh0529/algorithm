8f44452094833aa8c0834429b7937b93d5a19f84
chenhaoyang <chenhaoyang0529@gmail.com>
2016-11-08 00:10:21 +0800

    fix wrong answer

--- leetcode/happyNumber.js
+++ leetcode/happyNumber.js
@@ -1,26 +1,25 @@
-/**
- * @param {number} n
+;ram {number} n
  * @return {boolean}
  */
 var isHappy = function(n) {
-    
-    var splitNumber = function (number) {
-        var arr = []; 
-        while (number > 0) {
-            let remainder = number % 10;
-
-            arr.push(remainder);
-            number = Number.parseInt(number / 10, 10);
+	var count = 0,
+	    sum,
+	    remain;
+	    
+    while (count < 1000) {
+        count += 1;
+        sum = 0;
+        while (n > 0) {
+            remain = n % 10;
+            sum += remain * remain;
+            n = Number.parseInt(n / 10, 10);
         }
-        return arr;
-    };
-
-    var sumOfSquares = function (arr) {
-        return arr.length === 0 ? 0 : arr.reduce(function (pre, current) {
-            return Math.pow(pre, 2) + Math.pow(current, 2);
-        });
-    };
-    
-    var number = sumOfSquares(splitNumber(n));
-    return  number === 1 || isHappy(number);
+        if (sum === 1) {
+            return true;
+        }
+        else {
+            n = sum;
+        }
+    }
+    return false;
 };
