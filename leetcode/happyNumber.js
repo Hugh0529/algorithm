2c70e3668748066eb9665ccfc1fce73ce56b43bf
chenhaoyang <chenhaoyang0529@gmail.com>
2016-11-07 23:55:09 +0800

    Happy Number

--- leetcode/happyNumber.js
+++ leetcode/happyNumber.js
@@ -1,6 +1,26 @@
-ram {number} n
+/**
+ * @param {number} n
  * @return {boolean}
  */
 var isHappy = function(n) {
     
+    var splitNumber = function (number) {
+        var arr = []; 
+        while (number > 0) {
+            let remainder = number % 10;
+
+            arr.push(remainder);
+            number = Number.parseInt(number / 10, 10);
+        }
+        return arr;
+    };
+
+    var sumOfSquares = function (arr) {
+        return arr.length === 0 ? 0 : arr.reduce(function (pre, current) {
+            return Math.pow(pre, 2) + Math.pow(current, 2);
+        });
+    };
+    
+    var number = sumOfSquares(splitNumber(n));
+    return  number === 1 || isHappy(number);
 };
