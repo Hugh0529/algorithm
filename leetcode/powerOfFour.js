d72daae346bc74953b1f8319e789d4e3c8d096c8
chenhaoyang <chenhaoyang0529@gmail.com>
2016-11-09 23:58:08 +0800

    another way

--- leetcode/powerOfFour.js
+++ leetcode/powerOfFour.js
@@ -1,12 +1,9 @@
-/**
- * @param {number} num
+ram {number} num
  * @return {boolean}
  */
 var isPowerOfFour = function(num) {
-    num = num.toString(4);
-    var flag = 0;
-    for (let i = num.length - 1; i >= 0; i--) {
-        flag += Number(num.charAt(i));
+    while (num >= 4) {
+        num = num / 4;
     }
-    return flag === 1;
+    return num === 1;
 };
