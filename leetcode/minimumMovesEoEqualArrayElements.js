e796bfe7b03b9abb80a911904efe75114a94d73d
chenhaoyang <chenhaoyang0529@gmail.com>
2016-11-16 00:23:35 +0800

    Minimum Moves to Equal Array Elements

--- leetcode/minimumMovesEoEqualArrayElements.js
+++ leetcode/minimumMovesEoEqualArrayElements.js
@@ -1,7 +1,17 @@
-/**
- * @param {number[]} nums
+ram {number[]} nums
  * @return {number}
  */
 var minMoves = function(nums) {
+    let min = nums[0],
+        length = nums.length,
+        sum = min;
+        
+    for (let i = length - 1; i > 0; i--) {
+        let num = nums[i];
+        sum += num;
+        min = num < min ? num : min;
+    }
     
+    return sum - min * length;
 };
+
