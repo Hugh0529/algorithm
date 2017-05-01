ca43c314ccc37dff15e1673cb720e6bed37e216c
chenhaoyang <chenhaoyang0529@gmail.com>
2017-05-01 14:03:10 +0800

    update

--- leetcode/557.ReverseWordsinaStringIII.js
+++ leetcode/557.ReverseWordsinaStringIII.js
@@ -3,16 +3,31 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    var arr = s.split(' ');
-    for(let i = arr.length -1; i >= 0; i--) {
-        const item = arr[i];
-        let newItem = '';
-        for (let j = item.length - 1; j >= 0; j--) {
-            newItem += item.charAt(j);
+    var res = '';
+    var spaceFlag = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s.charAt(i) === ' ') {
+            spaceFlag.push(i);
         }
-        arr[i] = newItem;
-        
     }
-    return arr.join(' ');
+    spaceFlag.push(s.length);
+    var i = 0;
+    var length = spaceFlag.length;
+    while (i < length) {
+        const index = spaceFlag[i];
+        const preIndex = spaceFlag[i - 1] || 0;
+        var si;
+        for (let j = index - 1; j >= preIndex; j--) {
+            si = s.charAt(j);
+            if (si !== ' ') {
+                res = res + si;
+            }
+        }
+        if (i !== length - 1) {
+            res += ' ';
+        }
+        i++;
+    }
+    return res;
 };
 
