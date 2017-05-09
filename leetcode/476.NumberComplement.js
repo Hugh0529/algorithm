167ea84828e93b628cd232a298c71e828e69365d
chenhaoyang <chenhaoyang0529@gmail.com>
2017-05-09 16:26:12 +0800

    3

--- leetcode/476.NumberComplement.js
+++ leetcode/476.NumberComplement.js
@@ -1,13 +1,8 @@
-ram {number} num
+/**
+ * @param {number} num
  * @return {number}
  */
 var findComplement = function(num) {
-    var sum = 0;
-    var i = 0;
-    while (sum < num) {
-        sum += Math.pow(2, i);
-        i++;
-    }
-    return sum - num;
+    return Math.pow(2, num.toString(2).length) - num - 1;
 };
 
