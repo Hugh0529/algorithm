aeb15266a9704e625d7987003f23660b9bb083f5
chenhaoyang <chenhaoyang0529@gmail.com>
2017-05-13 23:45:02 +0800

    90.24%

--- leetcode/575.DistributeCandies.js
+++ leetcode/575.DistributeCandies.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var distributeCandies = function(candies) {
-    
+    var set = new Set(candies);
+    var l1 = candies.length;
+    var l2 = set.size;
+    var t = l1 / 2;
+    if (l2 > t) {
+        return t;
+    }
+    return l2;
 };
