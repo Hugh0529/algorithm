d1fa021aef95a35fb38449918b32be72e1a99637
chenhaoyang <chenhaoyang0529@gmail.com>
2017-05-26 23:57:38 +0800

    init

---
+++ leetcode/448.FindAllNumbersDisappearedInAnArray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    var res = [];
+    for (let i = nums.length - 1; i >= 0; i--) {
+        const t = nums[i] - 1;
+        if (t > 0) {
+            nums[i] 
+        }
+    }
+};
+
